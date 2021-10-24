7d1266adef555b6361bace771faef1a7c82bc986
dubey-shivani <shreyamishra040398@gmail.com>
2021-10-24 18:54:25 +0530

    fixing issue

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,6 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 
 app.use("/api/products/", productRoute);
-app.use("/api", productRoute);
 app.use("/api/users", userRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/upload", uploadRoute);
@@ -32,16 +31,21 @@ const __dirname = path.resolve();
 console.log("DIR", __dirname);
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+// ----------Point to index HTML in build Folder----------
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
-  app.get("/api", (req, res) => {
-    res.send("Api running");
+  app.get("/", (req, res) => {
+    res.send("API is running....");
   });
 }
+
+// ----------------------------------------------------
 app.use(NotFound);
 app.use(errorHandler);
 
