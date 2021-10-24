412a46181965779f4b432502e5ff8ef63d942a01
dubey-shivani <shreyamishra040398@gmail.com>
2021-10-24 22:56:26 +0530

    fixing issue

--- backend/server.js
+++ backend/server.js
@@ -18,22 +18,7 @@ if (process.env.NODE_ENV === "development") {
 }
 app.use(express.json());
 const __dirname = path.resolve();
-console.log("DIR", __dirname);
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
-
-// ----------Point to index HTML in build Folder----------
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
 
 app.use("/api/products/", productRoute);
 app.use("/api/users", userRoute);
@@ -44,7 +29,19 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYAL_CLIENT_ID);
 });
 
-// ----------------------------------------------------
+// -----------------------DEPLOYMENT----------------------------
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+// -----------------------DEPLOYMENT----------------------------
+
 app.use(NotFound);
 app.use(errorHandler);
 
