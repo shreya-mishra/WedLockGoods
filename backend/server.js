fa562b3aa6f7e1c54525ad51fa6d6eab1e342ad6
dubey-shivani <shreyamishra040398@gmail.com>
2021-10-26 02:23:51 +0530

    resolving issue

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ console.log("node env", __dirname + "/frontend/build");
 if (process.env.NODE_ENV === "production") {
   console.log(" in a iff");
 
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
